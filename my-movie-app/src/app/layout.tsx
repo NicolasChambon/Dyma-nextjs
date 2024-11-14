cd19f19a526b094f8b414ada1cc9229055eeb711
NicolasChambon <nico.chambon@gmail.com>
2024-11-14 14:19:10 +0100

    lesson 16 done

--- my-movie-app/src/app/layout.tsx
+++ my-movie-app/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import './globals.css';
+import './globals.scss';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { roboto, montserrat } from '@/fonts';
 
